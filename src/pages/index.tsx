649e6dd8c75827b2e15418e45c96c172f60abe4d
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2023-02-19 22:58:33 +0700

    Remove generate from index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,10 @@
-import { useEffect } from 'react';
 import type { NextPage } from 'next';
 
 import Header from 'components/Header';
 import Board from 'components/Board';
 import Keyboard from 'components/Keyboard';
-import { useSudokuStore } from 'lib/stores';
 
 const Home: NextPage = () => {
-  const generate = useSudokuStore((state) => state.generate);
-
-  useEffect(() => {
-    generate();
-  }, []);
-
   return (
     <>
       <Header />
