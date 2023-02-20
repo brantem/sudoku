8622837124d87b5ac7c633a9d71893eb3e089283
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2023-02-20 18:31:43 +0700

    Add Head to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,38 @@
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from 'next-themes';
+import { ThemeProvider, useTheme } from 'next-themes';
+import Head from 'next/head';
 
 import 'styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const { resolvedTheme } = useTheme();
+
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Sudoku" />
+        <meta name="keywords" content="Sudoku" />
+        <title>Sudoku</title>
+
+        <meta name="theme-color" content={resolvedTheme === 'dark' ? '#000000' : '#f5f5f5'} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+const MyAppWrapper = (props: AppProps) => {
   return (
     <ThemeProvider attribute="class">
-      <Component {...pageProps} />
+      <MyApp {...props} />
     </ThemeProvider>
   );
-}
+};
 
-export default MyApp;
+export default MyAppWrapper;
